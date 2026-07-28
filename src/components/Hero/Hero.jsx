479bb72d3eb4c2ab8fb69f31d77d75f9684bad02
Gilberto Santos <gfernandesdc@gmail.com>
2026-07-28 19:19:19 -0300

    Troca de frases da Hero da LP Humanologia

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,18 +11,12 @@ function Hero() {
             Humanologia Brasileira
           </span>
           <h1>
-            Sua vida tem uma origem, uma lógica, um porquê.
+            Descubra o que realmente está influenciando as decisões que você toma.
           </h1>
           <h2>
-            A Humanologia Brasileira ajuda você a compreender <br />a história que moldou a sua vida, identificar os padrões que influenciam suas escolhas e enxergar com mais clareza para tomar decisões mais maduras.
+            A Primeira Leitura da Humanologia Brasileira ajuda você a compreender a lógica da sua história e enxergar com mais clareza o momento que está vivendo, <br />para tomar decisões mais alinhadas<br /> com quem você realmente é.
           </h2>
-          <p>
-            <strong >Caminhamos com você na reorganização da sua vida e das suas relações, para que possa viver de forma mais coerente com quem você realmente é.
-          </strong>
-          </p>
-          <p className="hero-destaque">
-            Toda transformação começa quando compreendemos a nossa história <br />e encontramos o nosso lugar na vida.
-          </p>
+       
           <a
             href="https://docs.google.com/forms/d/e/1FAIpQLSfzP_KPKxGs4p1FMn7BTRcBNVsPc7vmJaBfRRtQBLMiYmsJHg/viewform"
             target="_blank"
